87701c92c3fdff82a692e9a2383368f4aeec7477
hongmin.zhang <hongmin.zhang>
2021-12-09 14:36:16 +0800

    first commit

---
+++ datatype/clob.go
@@ -0,0 +1,39 @@
+package datatype
+
+import (
+	"database/sql/driver"
+	"dmd/dm"
+	"errors"
+	"fmt"
+)
+
+type Clob string
+
+func (c *Clob) Scan(value interface{}) error {
+	clob, ok := value.(*dm.DmClob)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to parse dm clob value:", value))
+	}
+	length, err := clob.GetLength()
+	if err != nil {
+		return errors.New(fmt.Sprint("Failed to get dm clob length:", value))
+	}
+	if length == 0 {
+		*c = ""
+	} else {
+		s, err := clob.ReadString(1, int(length))
+		if err != nil {
+			return errors.New(fmt.Sprint("Failed to read dm clob string:", value))
+		}
+		*c = Clob(s)
+	}
+	return nil
+}
+
+func (c Clob) Value() (driver.Value, error) {
+	return string(c), nil
+}
+
+func (Clob) GormDataType() string {
+	return "text"
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module dmd
+
+go 1.16
+
+require (
+	github.com/emirpasic/gods v1.12.0
+	github.com/golang/snappy v0.0.4
+	github.com/thoas/go-funk v0.9.1
+	golang.org/x/text v0.3.7
+	gorm.io/gorm v1.22.4
+)
